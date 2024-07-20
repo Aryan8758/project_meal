c65eb498a3ffbc1b64ea9bed395cd40e1278e289
Aryan8758 <aryansingh8758@gmail.com>
2024-07-20 12:17:17 +0530

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        lateinit var handler: Handler
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this@MainActivity, home_page::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
