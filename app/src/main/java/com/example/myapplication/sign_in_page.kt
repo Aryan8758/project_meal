0aaed939a001db9df0ffea8a73f6c5d61a9a12af
Aryan8758 <aryansingh8758@gmail.com>
2024-07-22 14:18:41 +0530

    new_page commit

--- app/src/main/java/com/example/myapplication/sign_in_page.kt
+++ app/src/main/java/com/example/myapplication/sign_in_page.kt
@@ -1,11 +1,24 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class sign_in_page : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_page)
+        setContentView(R.layout.activity_sign_in_page)
+        val login: TextView = findViewById(R.id.already_joined)
+        login.setOnClickListener {
+            val intent = Intent(this@sign_in_page, sign_up_page::class.java)
+            startActivity(intent)
+            Toast.makeText(this, "Button clicked!", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
