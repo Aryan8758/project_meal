31e03c0aa51690098e8b547dd165ba2ecfbd73e3
Aryan8758 <aryansingh8758@gmail.com>
2024-07-26 13:23:00 +0530

    imgchange commit

--- app/src/main/java/com/example/meal_apk/HomePageActivity.kt
+++ app/src/main/java/com/example/meal_apk/HomePageActivity.kt
@@ -1,12 +1,16 @@
 package com.example.meal_apk
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.R
 
 class HomePageActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_page)
+
+
+
     }
 }
--- app/src/main/java/com/example/meal_apk/sign_in_page.kt
+++ app/src/main/java/com/example/meal_apk/sign_in_page.kt
@@ -1,16 +1,13 @@
 package com.example.meal_apk
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.R
-import com.example.myapplication.databinding.ActivityMainBinding
 
 class sign_in_page : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in_page)
